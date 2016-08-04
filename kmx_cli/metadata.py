813bae2d2917905ff589808f3dccefcaecc55971
zhengchunxi <zhengchunxi@foxmail.com>
2016-08-04 19:13:32 +0800

    refactor,format metadata

---
+++ kmx_cli/metadata.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from request import get,post
+from colorama import Fore, Back
+import json
+import sqlparse
+from sqlparse.tokens import DML, DDL, Keyword, Whitespace, Wildcard, Comparison
+from sqlparse.sql import IdentifierList, Identifier, Where
+from sqlparse.sql import Comparison as sqlcomp
+
+from pretty import pretty_meta, pretty_meta_list
+
+
+def query_meta(url,statement):
+    tokens = statement.tokens
+    if len(tokens) < 3 or tokens[0].value.strip().lower() != 'show' :
+        print Back.YEELOW + 'Please add table name in your sql. Table name show be in [devices ,device-type] ....'
+        return
+
+    params = tokens[2].value.strip().split(' ')
+    path = params[0].lower()
+
+    if path != 'devices'.lower() and path != 'device-types':
+        print ' Usage : show table [id] .   '
+        print 'Table show be in [ devices , device-type ] ....'
+        return
+    id = ''
+    if len(params) > 1 :
+        id = params[1]
+
+    uri = url + '/' + path + '/' + id
+    response = get(uri)
+    resopnse_payload = json.loads(response.text)
+
+    if len(params) > 1 :
+        path = path[:-1]
+        if '-' in path:
+            path = 'deviceType'
+        pretty_meta(resopnse_payload, path)
+    else:
+        pretty_meta_list(resopnse_payload, path)
+
+def parse_attr(payload, tokens):
+    length = len(tokens) + 1;
+    if length > 4:
+        for index in range(4, length, 2):
+            key = tokens[index][0].value.encode("utf-8").strip()
+            if key == 'tags':
+                payload['tags'] = tokens[index][1].value.encode("utf-8").strip()[1:-1].split(',')
+            elif key == 'attributes':
+                attributes = []
+                attrs = tokens[index][1].value[1:-1].split(',')
+                for att in attrs:
+                    attribute = {}
+                    items = att.encode("utf-8").strip().split(' ')
+                    attribute['name'] = items[0].strip()
+                    if len(items) >= 2:
+                        attribute['attributeValue'] = items[1].strip()
+                    else:
+                        print Back.YELLOW + 'attribute:' + items[0] + ' have no value...'
+                    attributes.append(attribute)
+                payload['attributes'] = attributes
+    return payload
+
+
+def create_meta(url, sql):
+    tokens = sql.tokens
+    path = tokens[2].value.encode("utf-8").lower().strip()
+
+    action = 'device'
+    uri = url + '/' + path
+
+    payload = {}
+    if path == 'device-types':
+        action = 'deviceType'
+        sensors = []
+        columns = tokens[4][1].value[1:-1].split(',')
+        for column in columns:
+            sensor = {}
+            items = column.encode("utf-8").strip().split(' ')
+            if len(items) < 2:
+                print Back.RED + 'sensor : ' + items[0] + ' should have valueType...'
+                return
+            sensor['id'] = items[0].strip()
+            sensor['valueType'] = items[1].strip().upper()
+            sensors.append(sensor)
+
+            payload['id'] = tokens[4][0].value.encode("utf-8").strip()
+            payload['sensors'] = sensors
+
+    elif path == 'devices':
+        payload['id'] = tokens[4][0].value.encode("utf-8").strip()
+        payload['deviceTypeId'] = tokens[4][1].value[1:-1].strip()
+
+    else:
+        print ' Table ERROR .. Table show be in [ device-types ,devices ]'
+        return
+    payload = parse_attr(payload, tokens)
+
+    response = post(uri, json.dumps(payload))
+    resopnse_payload = json.loads(response.text)
+    pretty_meta(resopnse_payload, action)
---
+++ kmx_cli/pretty.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+from tabulate import tabulate
+from colorama import Fore, Back
+
+
+def pretty_page(pages):
+    print 'total:%d    size:%d    pageNum:%d    pages:%d    pageSize:%d\n' % (pages['total'],pages['size'],pages['pageNum'],pages['pages'],pages['pageSize'])
+
+
+def pretty(dict, format='psql'):
+    ''' @param: query_result is a dict
+        @param: fmt may be 'plain', 'simple', 'grid', 'fancy_grid',
+                'psql', 'pipe', 'orgtbl', 'rst', 'html' etc
+                detail see https://pypi.python.org/pypi/tabulate
+    '''
+    result = []
+    headers = ['device', 'ts', 'sensorName', 'sensorValue']
+    non_exist = '-'  # show when key does not exist
+    err_msg = dict['message']
+
+    if 'dataRows' in dict.keys():
+        recs = dict['dataRows']
+        for rec in recs:
+            device = rec.get('device', non_exist)
+            ts = rec.get('iso', non_exist)
+            if 'dataPoints' in rec.keys():
+                sensor_recs = rec['dataPoints']
+                for sensor in sensor_recs:
+                    result.append((device, ts, sensor.get('sensor', non_exist), sensor.get('value', non_exist)))
+        if result:
+            print tabulate(result, headers, tablefmt=format)
+    elif 'dataPoints' in dict.keys():
+        for rec in dict['dataPoints']:
+            result.append((rec['device'], rec.get('timestamp', non_exist), rec.get('sensor', non_exist),rec.get('value', non_exist)))
+        if result:
+            print tabulate(result, headers, tablefmt=format)
+    print Fore.YELLOW + err_msg
+
+    if 'pageInfo' in dict:
+        pages = dict['pageInfo']
+        print 'size:%d    pageNum:%d    pageSize:%d\n' % (pages['size'],pages['pageNum'],pages['pageSize'])
+
+
+def pretty_meta_list(payload, action, format='psql'):
+    if not action:
+        print json.dumps(payload, sort_keys=True, indent=4) + '\n'
+        return
+
+    results = []
+    if action == 'device-types':
+        action = 'deviceTypes'
+    lists = payload[action]
+
+    if action == 'deviceTypes' :
+        headers = ['id','url']
+        for data in lists:
+            results.append((data['id'], data['url']))
+        print tabulate(results, headers, tablefmt=format)
+    else :
+        headers =  ['id','url','deviceTypeId','deviceTypeUrl']
+        for data in lists:
+            results.append( (data['id'], data['url'], data['deviceType']['id'], data['deviceType']['url']) )
+        print tabulate(results, headers, tablefmt=format)
+
+    pages = payload['pageInfo']
+    if pages:
+        pretty_page(pages)
+
+
+def pretty_meta(payload, path, format='psql'):
+    result = []
+    rows = []
+    sensors = []
+    headers = payload.keys()
+
+    if path in payload.keys() :
+        payload = payload[path];
+        sensors = payload.pop('sensors')
+        headers = payload.keys()
+
+    for header in headers:
+        rows.append(json.dumps(payload[header]))
+    result.append(tuple(rows))
+
+    if sensors:
+        sensorRows = []
+        keys = sensors[0].keys()
+        for sensor in sensors:
+            row = []
+            for key in keys:
+                row.append(sensor[key])
+            sensorRows.append(tuple(row))
+        print tabulate(sensorRows, keys, tablefmt=format)
+
+    print tabulate(result, headers, tablefmt=format)
+    print
