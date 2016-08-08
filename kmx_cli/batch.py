eb7b85ffd067053c14b5b60954cac0d7705c32fb
lzxking <157092540@qq.com>
2016-08-08 17:53:44 +0800

    add batch execution

--- kmx_cli/batch.py
+++ kmx_cli/batch.py
@@ -1,30 +1,75 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+
 import sqlparse
+
+import cli
+import log
+
+
 def get_batchparameters(sql):
-    sql_str=str(sql).lstrip().rstrip()
+    sql_str = str(sql).lstrip().rstrip()
     if sql_str.__contains__(' td'):
-        operation_str=sql_str[sql_str.find(' td'):].rstrip().lstrip()
-        terminator=operation_str[2]
-        script_path=operation_str[3:].rstrip().lstrip()
+        operation_str = sql_str[sql_str.find(' td'):].rstrip().lstrip()
+        terminator = operation_str[2]
+        script_path = operation_str[3:].rstrip().lstrip()
     else:
         script_path = sql_str[6:].rstrip().lstrip()
     return script_path
 
 
+def get_allfilepath(path):
+    files = []
+    for fpathe, dirs, fs in os.walk(path):
+        for f in fs:
+            if not f.endswith('~'):
+                files.append(os.path.join(fpathe, f))
+    return files
 
 
-
+def batch_exec(url,sql):
+    if str(sql).rstrip().lstrip().upper()=='SOURCE':
+        log.error('Please enter at least one script path after the keyword "SOURCE"')
+        log.info('Usage: SOURCE Script1,Script2...')
+        return
+    script_path = get_batchparameters(sql)
+    if (script_path).endswith(','):
+        script_path = script_path[:-1]
+    paths = script_path.split(',')
+    for path in paths:
+        if os.path.exists(path.lstrip().rstrip()):
+            if os.path.isfile(path.lstrip().rstrip()):
+                file_handler = open(path.lstrip().rstrip())
+                for line in file_handler:
+                    parsed = sqlparse.parse(line[:-1])
+                    log.info(line[:-1])
+                    cli.transfer(url,parsed)
+                file_handler.close()
+            elif os.path.isdir(path.lstrip().rstrip()):
+                files = get_allfilepath(path.lstrip().rstrip())
+                for file in files:
+                    if os.path.isfile(file.lstrip().rstrip()):
+                        file_handler = open(file.lstrip().rstrip())
+                        for line in file_handler:
+                            parsed = sqlparse.parse(line[:-1])
+                            log.info(line[:-1])
+                            cli.transfer(url,parsed)
+                        file_handler.close()
+        else:
+            log.error('Can not find the File/Dir : ' + path.lstrip().rstrip() + ' ,please check...')
 
 
 def test():
-    statements= sqlparse.parse("  source    /home/pc/Desktop/test.sql")
+    statements = sqlparse.parse("  source    /home/pc/Desktop/test.sql")
     for s in statements:
-       print get_batchparameters(s)
-
-if __name__=='__main__':
-
-   test()
+        print get_batchparameters(s)
 
 
+def test1():
+    get_allfilepath('/home/pc/Desktop/test')
 
+
+if __name__ == '__main__':
+
+    test1()
--- kmx_cli/cli.py
+++ kmx_cli/cli.py
@@ -3,67 +3,49 @@
 
 import argparse
 import cmd
-import os
 import socket
 
 import sqlparse
 from colorama import Back
 
 import importor
-import log
-from batch import get_batchparameters
 from identify import isDDL, isDML, isKeyword, isIdentifier, isIdentifierList
 from metadata import query_meta, create_meta
 from query import dyn_query
 
 
-class cli(cmd.Cmd):
+def transfer(url, statements):
+    for statement in statements:
+        if str(statement).upper() == 'BYE' or str(statement).upper() == 'EXIT':
+            print 'Exit KMX CLI ...'
+            return 'stop'
+        elif str(statement).lstrip().strip().upper().startswith('SOURCE'):
+            from batch import batch_exec
+            batch_exec(url,statement)
+        elif isDML(statement):
+            dyn_query(url, statement)
+        elif isDDL(statement):
+            create_meta(url, statement)
+        elif isKeyword(statement):
+            query_meta(url, statement)
+        elif isIdentifier(statement) or isIdentifierList:
+            importor.run(url, statement)
+        else:
+            print Back.RED + 'The input statement "' + str(statement) + '" is not supported ...' + Back.RESET
+        print
 
+
+
+class cli(cmd.Cmd):
     hostname = socket.gethostname();
     ip = socket.gethostbyname(hostname);
     prompt = '[' + hostname + '@' + ip + '] > '
 
-    def transfer(self, statements):
-        for statement in statements:
-            if str(statement).upper() == 'BYE' or str(statement).upper() == 'EXIT':
-                print 'Exit KMX CLI ...'
-                return 'stop'
-            elif isDML(statement):
-                dyn_query(self.url, statement)
-            elif isDDL(statement):
-                create_meta(self.url,statement)
-            elif isKeyword(statement):
-                query_meta(self.url,statement)
-            elif isIdentifier(statement) or isIdentifierList:
-                importor.run(self.url, statement)
-            else:
-                print Back.RED + 'The input statement "' + str(statement) + '" is not supported ...' + Back.RESET
-            print
-
     def onecmd(self, sql):
-        if (str(sql).lstrip().strip().upper()==("SOURCE")):
-            log.error('Please enter at least one script path after the keyword "SOURCE"')
-            log.info('Usage: SOURCE Script1,Script2...')
-            return
-        if (str(sql).lstrip().strip().upper().startswith("SOURCE")):
-            script_path=get_batchparameters(sql)
-            if(script_path).endswith(','):
-                script_path=script_path[:-1]
-            paths=script_path.split(',')
-            for path in paths:
-                if os.path.exists(path.lstrip().rstrip()):
-                    for line in open(path.lstrip().rstrip()):
-                        parsed = sqlparse.parse(line[:-1])
-                        log.info(line[:-1])
-                        self.transfer(parsed)
-                else:
-                    log.error('The file ' +path.lstrip().rstrip()+' not existing,please check...')
-
-        else:
-            parsed = sqlparse.parse(sql)
-            rc = self.transfer(parsed)
-            if rc == 'stop':
-                return True
+        parsed = sqlparse.parse(sql)
+        rc = transfer(self.url,parsed)
+        if rc == 'stop':
+            return True
 
 
 def run():
