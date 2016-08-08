d1e916c2bfcc92ccaf2dfa206576811462caf323
ChangXue <changxue@k2data.com.cn>
2016-08-08 17:43:53 +0800

    add log to cli, make transfer outside

--- kmx_cli/cli.py
+++ kmx_cli/cli.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+'''
+Usage:
+     e.g.
+     ./cli.py -u http://192.168.130.2/cloud/qa3/kmx/v2
+'''
 
 import sqlparse
 
-import argparse
 import socket
 import cmd
 
@@ -20,47 +24,40 @@ class cli(cmd.Cmd):
     ip = socket.gethostbyname(hostname);
     prompt = '[' + hostname + '@' + ip + '] > '
 
-    def transfer(self, statements):
-        for statement in statements:
-            if str(statement).upper() == 'BYE' or str(statement).upper() == 'EXIT':
-                print 'Exit KMX CLI ...'
-                return 'stop'
-            elif isDML(statement):
-                dyn_query(self.url, statement)
-            elif isDDL(statement):
-                create_meta(self.url,statement)
-            elif isKeyword(statement):
-                query_meta(self.url,statement)
-            elif isIdentifier(statement) or isIdentifierList:
-                importor.run(self.url, statement)
-            else:
-                print Back.RED + 'The input statement "' + str(statement) + '" is not supported ...' + Back.RESET
-            print
-
     def onecmd(self, sql):
         statements = sqlparse.parse(sql.strip(),'utf-8')
-        rc = self.transfer(statements)
+        rc = transfer(statements)
         if rc == 'stop':
             return True
 
+def transfer(url, statements):
+    for statement in statements:
+        if str(statement).upper() in ('BYE', 'EXIT'):
+            log.default('Exit KMX CLI ...')
+            return 'stop'
+        elif isDML(statement):
+            dyn_query(url, statement)
+        elif isDDL(statement):
+            create_meta(url,statement)
+        elif isKeyword(statement):
+            query_meta(url,statement)
+        elif isIdentifier(statement) or isIdentifierList:
+            importor.run(url, statement)
+        else:
+            log.error('The input statement "' + str(statement) + '" is not supported ...')
+        print
 
-def run():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-u', '--url', help='Input HTTP REST URL of your KMX query engine.')
-    args = parser.parse_args()
-    url = args.url
-
-    if url:
-        print 'Query URL: ' + Back.GREEN + str(url) + Back.RESET
-        client = cli()
-        client.url = url
-        client.cmdloop()
-
-    else:
-        print 'You must provide an HTTP REST URL for KMX query ...'
-        print 'Use -u or --url to init URL'
-        print 'Use -h to get help ...'
+def run(url):
+    log.primary('Query URL: ' + str(url))
+    client = cli()
+    client.url = url
+    client.cmdloop()
 
 
 if __name__ == '__main__':
-    run()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--url', required=True, help='Input HTTP REST URL of your KMX query engine.')
+    args = parser.parse_args()
+
+    run(args.url)
