a37d27e0d82b6c8b6356c903c29117303baab588
zhengchunxi <zhengchunxi@foxmail.com>
2016-08-11 10:54:32 +0800

    update testcase for create

--- test/test_create_mete.py
+++ test/test_create_mete.py
@@ -11,12 +11,13 @@ import time
 cur_dir = os.path.abspath(os.path.dirname(__file__))
 path = os.path.abspath(os.path.join(cur_dir, '../kmx_cli'))
 sys.path.append(path)
-import create,metadata,cli
+import create,metadata,cli,log
 
 
 class Tester(unittest.TestCase):
 
     def setUp(self):
+        log.primary('================================================== setUp ==================================================')
         self.url = 'http://192.168.130.2/cloud/qa3/kmx/v2'
         self.num = str(random.randint(10000, 99999))
         self.device_type = ['test_dt1_%s' % self.num, 'test_dt2_%s' % self.num]
@@ -33,6 +34,7 @@ class Tester(unittest.TestCase):
 
     def tearDown(self):
         time.sleep(20)
+        log.primary('================================================== tearDown ==================================================')
         deletes = [
                     'drop device ' + self.device,
                     'drop devicetype ' + self.device_type[0],
@@ -42,10 +44,12 @@ class Tester(unittest.TestCase):
         cli.transfer(self.url, statements)
 
     def test_create_device(self):
+        log.primary('================================================== test create device ==================================================')
         sql = 'create device ' + self.device + '(' + self.device_type[0] + ') tags(t1,t2,标签) attributes(属性 属性值,k2 v2)'
         self.execute(sql, is_assert=True)
 
     def test_create_device_type(self):
+        log.primary('================================================== test create deviceType ==================================================')
         sql = 'create deviceType ' + self.device_type[1] + '(s1 String,s2 Float) tags(t1,t2,标签) attributes(属性 属性值,k2 v2)'
         self.execute(sql, is_assert=True)
 
