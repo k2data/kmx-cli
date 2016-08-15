63e2df9c83b8f5562aaaca146440d7bfd43e7d3b
zhengchunxi <zhengchunxi@foxmail.com>
2016-08-15 11:52:10 +0800

    add statistic.py

---
+++ kmx_cli/statistic.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pandas
+import numpy
+import pylab
+import json
+from request import get
+
+
+def parse_payload(payload, sensors):
+    index = []
+    values = {}
+    for sensor in sensors:
+        values[sensor] = []
+
+    data_rows = json.loads(payload)['dataRows']
+    for data_row in data_rows:
+        index.append(data_row['iso'])
+        data_points = data_row['dataPoints']
+        for data_point in data_points:
+            sensor = data_point['sensor']
+            if data_point.has_key('value') and data_point['value'] != 'null':
+                values[sensor].append(data_point['value'])
+            else:
+                values[sensor].append(None)
+    return index, values
+
+
+def get_data(sensors, values):
+    data_type = 'float64'
+    data = {}
+    for sensor in sensors:
+        data[sensor] = numpy.array(values[sensor], dtype=data_type)
+    return data
+
+
+def get_data_frame_data(payload, sensors):
+    index, values = parse_payload(payload, sensors)
+    data = get_data(sensors,values)
+    return pandas.DataFrame(data=data, index=index, columns=sensors)
+
+
+def describe(payload, sensors):
+    data_frame = get_data_frame_data(payload, sensors)
+    print data_frame.describe()
+
+
+def plot(payload, sensors):
+    data_frame = get_data_frame_data(payload, sensors)
+    data_frame.plot()
+    pylab.show()
+
+
+def box(payload, sensors):
+    data_frame = get_data_frame_data(payload, sensors)
+    # data_frame.boxplot(return_type='dict')
+    data_frame.boxplot(return_type='axes')
+    pylab.show()
+
+
+if __name__ == '__main__':
+    url = 'http://192.168.130.2/cloud/qa3/kmx/v2/data/data-rows?' +\
+          'select={"sources":{"device":"C302D0_10",' + \
+          '"sensors":["engineTemperature","enginRotate"],' + \
+          '"timeRange":{"start":{"iso":"1970-01-01T00:00:00.001-00:00"},"end":{"iso":"2016-08-15T09:44:55.687%2B08:00"}}}}' + \
+          '&size=5'
+    response = get(url)
+    response_payload = response.text
+    response.close()
+    sensor_ids = ["engineTemperature", "enginRotate"]
+
+    describe(response_payload, sensor_ids)
+    plot(response_payload, sensor_ids)
+    box(response_payload, sensor_ids)
