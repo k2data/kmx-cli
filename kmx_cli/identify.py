c42428396db04b23a1e7754550a4edeafc2a81e2
yangrui <yangrui.ruc@qq.com>
2016-08-29 10:48:38 +0800

    add two finders

--- kmx_cli/identify.py
+++ kmx_cli/identify.py
@@ -126,22 +126,98 @@ def find_next_token_by_ttype(sql, lambda_func, target_ttype):
             # print target
     return source_token,target
 
+
+def find_next_token(sql, lambda_func):
+    '''
+    import sqlparse
+    parsed = sqlparse.parse("select longitudeNum,T1,T2 from C2063B")
+    source,target = find_next_token(parsed[0], lambda t: t.value.upper() == 'C2063B')
+    :param sql:
+    :param lambda_func:
+    :return:
+    '''
+    target = ''
+    tokens = TokenList(sql.tokens)
+    if isinstance(tokens, TokenList):
+        tokens = list(tokens.flatten())
+        tokens = TokenList(tokens)
+
+        source_token = tokens.token_matching(lambda_func, 0)
+        if source_token:
+            source_idx = tokens.token_index(source_token, 0)
+            start = source_idx + 1
+            find = True
+            if start == len(list(tokens)):
+                return source_token,None
+            from sqlparse.tokens import Whitespace
+            while tokens[start].ttype is Whitespace:
+                if start < len(list(tokens)) - 1:
+                    start += 1
+                else:
+                    find = False
+                    break
+            # print start
+            if find:
+                target = tokens[start].value
+            else:
+                target = None
+            # print source_token
+            # print target
+    return source_token,target
+
+
+def find_next_token_util_stop_sign(sql, lambda_func, stop_sign):
+    '''
+    import sqlparse
+    from sqlparse.tokens import Literal
+    parsed = sqlparse.parse("select longitudeNum,T1,T2 from C2063B")
+    source1,target1 = find_next_token_util_stop_sign(parsed[0], lambda t: t.value.upper() == 'FROM', "where,100")
+    :param sql:
+    :param lambda_func:
+    :return:
+    '''
+    stop_sign_list = stop_sign.split(",")
+    target = ''
+    tokens = TokenList(sql.tokens)
+    if isinstance(tokens, TokenList):
+        tokens = list(tokens.flatten())
+        tokens = TokenList(tokens)
+
+        source_token = tokens.token_matching(lambda_func, 0)
+        if source_token:
+            source_idx = tokens.token_index(source_token, 0)
+            start = source_idx + 1
+            # find = True
+            from sqlparse.tokens import Whitespace
+            target = []
+            while tokens[start].value.upper() not in (sign.upper() for sign in stop_sign_list) or tokens[start].ttype is Whitespace:
+                # if start <= len(list(tokens)) - 1:
+                if tokens[start].ttype is not Whitespace:
+                    target.append(tokens[start].value)
+                if start == len(list(tokens)) - 1:
+                    break
+                start += 1
+                # else:
+                #     # find = False
+                #     break
+            # print start
+            # if find:
+            #     # target = tokens[start].value
+            #     pass
+            # else:
+            #     target = None
+            # print source_token
+            # print target
+    return source_token,target
+
+
 if __name__ == '__main__':
     import sqlparse
-    '''    statements = sqlparse.parse('import data.csv into test_deviceType;select * from devices;create devices id(deviceTypeId) tags(t1,t2) attributes(k1 v1,k2 v2);show devices;testdcz; dafs a')
-    for statement in statements:
-        print '"' + str(statement.tokens[0]) + '" isDML : ' + str(isDML(statement))
-        print '"' + str(statement.tokens[0]) + '" isDDL : ' + str(isDDL(statement))
-        print '"' + str(statement.tokens[0]) + '" isKeyword : ' + str(isKeyword(statement))
-        print '"' + str(statement.tokens[0]) + '" isIdentifier : ' + str(isIdentifier(statement))
-        print '"' + str(statement.tokens[0]) + '" isIdentifierList : ' + str(isIdentifierList(statement))
-        print
-    '''
-    s='''where key1=value1 and key2 = value2 and key3="value3" and "key4"=value4 and "key5" = "value5";'''
-    st = sqlparse.parse(s)[0]
-    print st.tokens
-    print
-    sss = reformat_tokens(st)
-    print sss
-    for index in xrange(1, len(sss), 3):
-        print index, sss[index]
+    # parsed = sqlparse.parse("select longitudeNum,T1,T2 from C2063B where ts>'now-3d' and ts < 'now' size 30 page 2 size 10 limit 11")
+    parsed = sqlparse.parse("select longitudeNum,T1,T2 from C2063B")
+    source,target = find_next_token(parsed[0], lambda t: t.value.upper() == 'C2063B')
+    print source
+    print target
+    source1,target1 = find_next_token_util_stop_sign(parsed[0], lambda t: t.value.upper() == 'FROM', "where,100")
+    print source1
+    print target1
